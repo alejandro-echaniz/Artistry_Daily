1d8556f10b3b6ea78470fe2bc499cd3f3f124510
Alejandro Echaniz <aecha@wireless-10-104-121-61.umd.edu>
2024-05-12 17:24:21 -0400

    adding node packages & organizing file structure

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
-
+/* importning necessary libraries */ 
+const express = require('express');
+const fs = require("fs");
+const path = require('path');
+const bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
 
